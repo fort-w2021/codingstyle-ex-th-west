dc265e255a6896ecfdae656b0e4def334ad5f218
th-west <twestermeier@live.de>
2020-11-05 16:13:50 +0100

    not done

--- refactor-dry-ex-sol.R
+++ refactor-dry-ex-sol.R
@@ -3,10 +3,22 @@ y <- rnorm(100)
 z <- rnorm(100)
 v <- z^2
 
+# Function that computes a 95% Confidence Interval given a numeric vector
 confidence_interval <- function(data) {
+  if (!checkmate::test_atomic_vector(data)) {
+    stop("supplied data is not a vector")
+  }
+  if (!checkmate::test_numeric(data)) {
+    stop("supplied data is not numeric")
+  }
   n <- length(data)
-  emp_mean <- mean(data)
-  emp_std_dev <- sd(data)
+  emp_mean <- mean(data, na.rm = TRUE)
+  emp_std_dev <- sd(data, na.rm = TRUE)
   half_width <- 1.96 * emp_std_dev / sqrt(n)
   c(emp_mean - half_width, emp_mean + half_width)
 }
+
+confidence_interval(x)
+confidence_interval(y)
+confidence_interval(z)
+confidence_interval(v)
